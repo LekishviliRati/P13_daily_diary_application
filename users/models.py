6bd1bf01798d376ca63d50c00a9f383ce6a8da31
LekishviliRati <lekishvili.rati@gmail.com>
2022-01-16 21:26:50 +0100

    Resize images with Pillow and Update profile iformations

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 350 or img.width > 350:
+            output_size = (350, 350)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
