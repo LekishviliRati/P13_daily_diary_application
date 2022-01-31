0708cc21f0eb375a5fc954304874e288933ed6f5
LekishviliRati <lekishvili.rati@gmail.com>
2022-01-31 12:14:56 +0100

    difference betwenn class based view and function based view

--- diary/urls.py
+++ diary/urls.py
@@ -12,7 +12,8 @@ from .views import (
 urlpatterns = [
     path('', views.home, name='diary-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    # path('post/', views.post, name='diary-post'),
+    # Function based view  : path('post/', views.post, name='diary-post'),
+    # Class based view : path('post/', PostListView.as_view(), name='diary-post'),
     path('post/', PostListView.as_view(), name='diary-post'),
     path('post/article/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/article/new/', PostCreateView.as_view(), name='post-create'),
