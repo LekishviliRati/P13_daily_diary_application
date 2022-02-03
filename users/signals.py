a126f655d2dfab8921971eae54eeb61ba9f944da
LekishviliRati <lekishvili.rati@gmail.com>
2022-02-03 21:47:46 +0100

    Add comments to siganls.py

--- users/signals.py
+++ users/signals.py
@@ -4,12 +4,14 @@ from django.dispatch import receiver
 from .models import Profile
 
 
+# If one user is created, one profile will be created as well
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
+# Save profile in DB
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
