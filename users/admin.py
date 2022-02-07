bd0e8c78c25f77a10a21834abc18675cf906ba44
LekishviliRati <lekishvili.rati@gmail.com>
2022-02-07 17:11:12 +0100

    Relatives are linked to Residents in DB

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Resident
+from .models import Profile, Resident, Relative
 
 admin.site.register(Profile)
 admin.site.register(Resident)
+admin.site.register(Relative)
 
--- users/models.py
+++ users/models.py
@@ -8,6 +8,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
+    class Meta:
+        ordering = ['user']
+
     def __str__(self):
         return f'{self.user.username} Profile'
 
@@ -23,17 +26,19 @@ class Profile(models.Model):
 
 
 class Resident(models.Model):
-    first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
+    first_name = models.CharField(max_length=40)
     birth_date = models.DateField()
     place_of_birth = models.CharField(max_length=50)
-    relatives = models.TextField()
     secret_information = models.TextField()
     surgeries = models.TextField()
     treating_doctor = models.CharField(max_length=250)
     medical_treatment = models.TextField()
     entry_date = models.DateField(default=timezone.now)
     image = models.ImageField(default='default.jpg', upload_to='resident_pics')
+
+    class Meta:
+        ordering = ['last_name']
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} '
@@ -47,3 +52,20 @@ class Resident(models.Model):
             output_size = (350, 350)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+class Relative(models.Model):
+    last_name = models.CharField(max_length=40)
+    first_name = models.CharField(max_length=40)
+    phone_number = models.CharField(max_length=40)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=150)
+    # relatives = models.ForeignKey(Resident, default=1, on_delete=models.CASCADE)
+    relatives = models.ManyToManyField(Resident)
+
+    class Meta:
+        ordering = ['last_name']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
